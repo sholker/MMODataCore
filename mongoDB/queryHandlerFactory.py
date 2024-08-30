15ab38784bd1ebe8d3ee0405771537a0dcbfdce4
sholker <oris1002@gmail.com>
2024-08-30 18:04:31 +0300

    uses in Enume collection name

--- mongoDB/queryHandlerFactory.py
+++ mongoDB/queryHandlerFactory.py
@@ -1,13 +1,13 @@
 from mongoDB.queryHandler import BaseQueryHandler,PlayerQueryHandler, PlayerActionQueryHandler, QuestQueryHandler
-
+from mongoDB.enums import CollectionType
 class QueryHandlerFactory:
     @staticmethod
     def get_query_handler(collection_name: str) -> BaseQueryHandler:
-        if collection_name == "player":
+        if collection_name == CollectionType.PLAYER.value:
             return PlayerQueryHandler()
-        elif collection_name == "player_action":
+        elif collection_name == CollectionType.PLAYER_ACTION.value:
             return PlayerActionQueryHandler()
-        elif collection_name == "quest":
+        elif collection_name == CollectionType.QUEST.value:
             return QuestQueryHandler()
         else:
             raise ValueError(f"Unknown collection name: {collection_name}")
